41716b8762c978c5f87a37c2d451a2926c4e8ca8
Ricky Hoberecht <richard.hoberecht@infineon.com>
2025-07-03 12:01:03 -0700

    update formal verification bits

--- src/dsm_buffer/dsm_model.v
+++ src/dsm_buffer/dsm_model.v
@@ -9,22 +9,38 @@
 
 // Calcualte CLOG2 up to 16 bits
 `define CLOG2(x) \
-   (x <= 2) ? 1 : \
-   (x <= 4) ? 2 : \
-   (x <= 8) ? 3 : \
-   (x <= 16) ? 4 : \
-   (x <= 32) ? 5 : \
-   (x <= 64) ? 6 : \
-   (x <= 128) ? 7 : \
-   (x <= 256) ? 8 : \
-   (x <= 512) ? 9 : \
-   (x <= 1024) ? 10 : \
-   (x <= 2048) ? 11 : \
-   (x <= 4096) ? 12 : \
-   (x <= 8192) ? 13 : \
-   (x <= 16384) ? 14 : \
-   (x <= 32768) ? 15 : \
-   (x <= 65535) ? 16 : \
+  (x <= 2) ? 1 : \
+  (x <= 4) ? 2 : \
+  (x <= 8) ? 3 : \
+  (x <= 16) ? 4 : \
+  (x <= 32) ? 5 : \
+  (x <= 64) ? 6 : \
+  (x <= 128) ? 7 : \
+  (x <= 256) ? 8 : \
+  (x <= 512) ? 9 : \
+  (x <= 1024) ? 10 : \
+  (x <= 2048) ? 11 : \
+  (x <= 4096) ? 12 : \
+  (x <= 8192) ? 13 : \
+  (x <= 16384) ? 14 : \
+  (x <= 32768) ? 15 : \
+  (x <= 65536) ? 16 : \
+  (x <= 131072) ? 17 : \
+  (x <= 262144) ? 18 : \
+  (x <= 524288) ? 19 : \
+  (x <= 1048576) ? 20 : \
+  (x <= 2097152) ? 21 : \
+  (x <= 4194304) ? 22 : \
+  (x <= 8388608) ? 23 : \
+  (x <= 16777216) ? 24 : \
+  (x <= 33554432) ? 25 : \
+  (x <= 67108864) ? 26 : \
+  (x <= 134217728) ? 27 : \
+  (x <= 268435456) ? 28 : \
+  (x <= 536870912) ? 29 : \
+  (x <= 1073741824) ? 30 : \
+  (x <= 2147483648) ? 31 : \
+  (x <= 4294967296) ? 32 : \
    -1
 
 module dsm_model(
@@ -52,12 +68,12 @@ module dsm_model(
     `endif
 );
 
-localparam MOD_BITS = 4; // Modulator Bits
-localparam SAMPLES  = 256; // 
+parameter MOD_BITS = 4; // Modulator Bits
+parameter SAMPLES  = 256; // 
 
 localparam BUFFER_SIZE = (SAMPLES - 1);
 localparam MOD_BITS_MSB = MOD_BITS - 1;
-localparam POINTER_BITS = ((`CLOG2(256)) - 1);
+localparam POINTER_BITS = ((`CLOG2(SAMPLES)) - 1);
 
 // 4 bits for PACSSv1, just 1 bit for PACSSv2 or CAFE
 reg [MOD_BITS_MSB:0] dsm_out;
@@ -124,10 +140,8 @@ end
 
 always @(posedge external_clk)
 begin
-    assume(internal_clk == 0);
     assume(internal_rst_n == 1);
     assume(external_rst_n == 1);
-    assume(save_index >= 0);
 
     if ($past(internal_clk) == 1)
         assert(save_index > 0);
@@ -135,22 +149,19 @@ end
 
 always @(posedge internal_clk)
 begin
-    assume(external_clk == 0);
     assume(internal_rst_n == 1);
-    assume(internal_rst_n == 1);
+    assume(external_rst_n == 1);
     assume(save_index > 0);
 
     assert(save_index > read_index);
 end
 
-// I wanna check that no read pointer errors occur. 
 always @(*)
 begin
     assume(internal_rst_n == 1);
     assume(external_rst_n == 1);
 
-    assume(read_index == 0);
-    assume(save_index > 0);
+    assume(save_index > read_index);
 
     assert(read_error == 0);
 end
